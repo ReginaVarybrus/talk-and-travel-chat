3ec5e40d83b39bbcf46460106e6b5ea0ab4a90f7
Regina Varybrus <varybrusr@gmail.com>
2024-01-15 23:40:16 +0000

    change basename

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <GlobalStyles />
-    <BrowserRouter basename="/chat-project">
+    <BrowserRouter basename="/talk-and-travel-chat">
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <App />
