035d46c9f7982167ec4a5ab0fb4787748148fbe0
Regina Varybrus <varybrusr@gmail.com>
2024-02-15 19:52:54 +0000

    add vitest library

---
+++ src/components/SearchInput/__tests__/SearchInput.spec.jsx
@@ -0,0 +1,5 @@
+import { describe, mount } from 'vitest';
+import { render } from '@testing-library/react';
+import SearchInput from '../SearchInput.jsx';
+
+describe('search input testing', () => {});
