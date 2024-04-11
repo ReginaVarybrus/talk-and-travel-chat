ed83e6eee447128ebcc936036c73df40e8db53f8
Regina Varybrus <varybrusr@gmail.com>
2024-04-11 16:29:49 +0100

    more changes in the Chat component

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,35 +1,11 @@
 import { useSelector } from 'react-redux';
 import { getCountryName } from '@/redux-store/selectors.js';
-import {
-  ChatStyled,
-  Header,
-  HeaderContent,
-  MessageBlock,
-  MessageBarWrapper,
-  MessageBar,
-  ButtonAttachFile,
-  VisuallyHiddenInput,
-  TextareaAutosize,
-  ButtonSendMessage,
-} from './ChatStyled';
-import Icons from '../Icons/Icons';
-import {
-  getCountryName,
-  getNumberOfParticipants,
-} from '@/redux-store/AuthOperations/selectors.js';
 import { ChatStyled, Header, HeaderContent, MessageBlock } from './ChatStyled';
+
 import MessageBar from '../MessageBar/MessageBar';
 
 const Chat = () => {
   const countryName = useSelector(getCountryName);
-  const [value, setValue] = useState('');
-  const textAreaRef = useRef(null);
-  const isInputNotEmpty = Boolean(value?.trim().length);
-
-  const handleChange = e => {
-    setValue(e.target.value);
-  };
-  const participants = useSelector(getNumberOfParticipants);
 
   return (
     <ChatStyled>
