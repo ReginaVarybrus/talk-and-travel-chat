78634ee067ff2ce38b417c85c988f3f8012a2ba5
Regina Varybrus <varybrusr@gmail.com>
2024-08-06 15:34:14 +0100

    started to add styles to the tapbar

--- src/components/TapBar/TapBar.jsx
+++ src/components/TapBar/TapBar.jsx
@@ -1,8 +1,8 @@
 import { useState, useRef } from 'react';
 
 import CssBaseline from '@mui/material/CssBaseline';
-import BottomNavigation from '@mui/material/BottomNavigation';
-import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+// import BottomNavigation from '@mui/material/BottomNavigation';
+// import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 // import RestoreIcon from '@mui/icons-material/Restore';
 // import FavoriteIcon from '@mui/icons-material/Favorite';
 // import ArchiveIcon from '@mui/icons-material/Archive';
@@ -12,9 +12,15 @@ import Paper from '@mui/material/Paper';
 // import ListItemAvatar from '@mui/material/ListItemAvatar';
 // import ListItemText from '@mui/material/ListItemText';
 // import Avatar from '@mui/material/Avatar';
+// import { MdMoreHoriz } from 'react-icons/md';
 
-import { RoomsIcon, DMsIcon, LogoutIcon } from '../SideBar/SideBarStyled.js';
-import { TapBarStyled } from './TapBarStyled.js';
+import { RoomsIcon, DMsIcon } from '../SideBar/SideBarStyled.js';
+import {
+  TapBarStyled,
+  TapBarButtonsWrapper,
+  TapBarButton,
+  MoreIcon,
+} from './TapBarStyled.js';
 
 // function refreshMessages() {
 //   const getRandomInt = max => Math.floor(Math.random() * Math.floor(max));
@@ -51,17 +57,17 @@ const TapBar = () => {
         sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }}
         elevation={3}
       >
-        <BottomNavigation
+        <TapBarButtonsWrapper
           showLabels
           value={value}
           onChange={(event, newValue) => {
             setValue(newValue);
           }}
         >
-          <BottomNavigationAction label="DMs" icon={<RoomsIcon />} />
-          <BottomNavigationAction label="Rooms" icon={<DMsIcon />} />
-          <BottomNavigationAction label="More" icon={<LogoutIcon />} />
-        </BottomNavigation>
+          <TapBarButton label="DMs" icon={<DMsIcon />} />
+          <TapBarButton label="Rooms" icon={<RoomsIcon />} />
+          <TapBarButton label="More" icon={<MoreIcon />} />
+        </TapBarButtonsWrapper>
       </Paper>
     </TapBarStyled>
   );
--- src/components/TapBar/TapBarStyled.js
+++ src/components/TapBar/TapBarStyled.js
@@ -1,10 +1,27 @@
 import styled from 'styled-components';
 import { device } from '@/constants/mediaQueries';
 import Box from '@mui/material/Box';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import { MdMoreHoriz } from 'react-icons/md';
 
 export const TapBarStyled = styled(Box)`
   z-index: 1;
   @media ${device.tablet} {
     display: none;
   }
+`;
+
+export const TapBarButtonsWrapper = styled(BottomNavigation)`
+  heigth: 100px;
+`;
+
+export const TapBarButton = styled(BottomNavigationAction)`
+  heigth: 100px;
+`;
+
+export const MoreIcon = styled(MdMoreHoriz)`
+  width: 48px;
+  height: 36px;
+  stroke: var(--color-grey-9);
 `;
