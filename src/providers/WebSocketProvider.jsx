d31810e713680155edb708edf89de34e7450d10e
Regina Varybrus <varybrusr@gmail.com>
2024-08-30 19:08:47 +0100

    added autorizing websocket connection

--- src/providers/WebSocketProvider.jsx
+++ src/providers/WebSocketProvider.jsx
@@ -1,4 +1,3 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
 import { StompSessionProvider } from 'react-stomp-hooks';
 import { useSelector } from 'react-redux';
 import { getToken } from '@/redux-store/selectors.js';
