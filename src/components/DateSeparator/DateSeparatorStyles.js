fcc4ed84a0c23228349548cbbb35310ec26880d9
ValentinaHotsa <134721626+ValentinaHotsa@users.noreply.github.com>
2024-09-27 11:59:13 +0200

    fix style bag

--- src/components/DateSeparator/DateSeparatorStyles.js
+++ src/components/DateSeparator/DateSeparatorStyles.js
@@ -27,7 +27,7 @@ export const Line2 = styled.div`
   background-color: var(--color-grey-6);
   margin: 0 24px 0 16px;
   @media ${device.tablet} {
-    margin: 0 16px 0 32px;
+    margin: 0 32px 0 16px;
   }
 `;
 
