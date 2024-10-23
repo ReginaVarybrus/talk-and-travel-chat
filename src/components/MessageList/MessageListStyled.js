e5bcd95aba4aa6c48c0ac63be2bf6ad6f750c9c2
ValentinaHotsa <134721626+ValentinaHotsa@users.noreply.github.com>
2024-10-23 12:12:00 +0200

    clean/delete commented string

--- src/components/MessageList/MessageListStyled.js
+++ src/components/MessageList/MessageListStyled.js
@@ -1,6 +1,3 @@
 import styled from 'styled-components';
 
-export const MessageListStyled = styled.ul`
-  //   display: flex;
-  //   flex-direction: column-reverse;
-`;
+export const MessageListStyled = styled.ul``;
