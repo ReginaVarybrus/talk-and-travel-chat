477b4a8b83be69b46c412c5acb6a8b7973a4e3b0
Regina Varybrus <78879888+ReginaVarybrus@users.noreply.github.com>
2024-11-15 16:50:16 +0000

    KAN-189-Add-popup-to-the-map (#106)
    * popover has been added
    
    * merge main

--- src/components/MessageBar/MessageBarStyled.js
+++ src/components/MessageBar/MessageBarStyled.js
@@ -19,6 +19,7 @@ export const ButtonJoinWrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  margin-bottom: 32px;
 `;
 
 export const MessageInputs = styled.form`
