6aa97bbbe5017401da768bb1443e2b370be11de7
ValentinaHotsa <134721626+ValentinaHotsa@users.noreply.github.com>
2024-12-05 14:41:31 +0100

    try change login with google

--- src/components/Buttons/GoogleButton/GoogleButton.jsx
+++ src/components/Buttons/GoogleButton/GoogleButton.jsx
@@ -49,7 +49,7 @@ const ButtonGoogle = () => {
           '853304957930-2cclc0tr0hs9l4m918bgoeg51t8ca5u5.apps.googleusercontent.com',
 
         callback: handleCredentialResponse,
-        ux_mode: 'popup',
+        ux_mode: 'redirect',
       });
     } else {
       console.error('Google API is not loaded');
